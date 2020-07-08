20c9231db9fa1e2052b476d76ffb26bb6fba286b
AmanKumar26963 <amankumar2696@gmail.com>
2020-07-08 12:24:26 +0530

    FetchPolicies Script

--- Policies/FetchPolicies.go
+++ Policies/FetchPolicies.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	cmd, _ := exec.Command("bash", "script.sh").Output()
-	fmt.Println(string(cmd))
+	//cmd, _ := exec.Command("bash", "script.sh").Output()
+	//fmt.Println(string(cmd))
+	get()
+}
 
+func get() {
+	path, _ := exec.LookPath("bash")
+	cmd := &exec.Cmd{
+		Path:   path,
+		Args:   []string{"./", "script.sh"},
+		Stdout: os.Stdout,
+		Stdin:  os.Stdin,
+	}
+	cmd.Start()
+	cmd.Wait()
 }
--- Policies/script.sh
+++ Policies/script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "Policies: "
+kubectl get cnp
+echo
 kubectl describe cnp gcloudpolicy
 
